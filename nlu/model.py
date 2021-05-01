306a118081759945d16aeebb99cf39e19a1b4594
BacalhauSalgado <81281788+BacalhauSalgado@users.noreply.github.com>
2021-05-01 17:53:05 -0300

    Create model.py

---
+++ nlu/model.py
@@ -0,0 +1,28 @@
+import yaml
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Embedding
+from tensorflow.keras.utils import to_categorical
+
+data = yaml.safe_load(open('nlu\\train.yml', 'r', encoding='utf-8').read())
+
+inputs, outputs = [], []
+
+for command in data['commands']:
+    inputs.append(command['input'].lower())
+    outputs.append('{}\{}'.format(command['entity'], command['action']))
+
+
+# Processar texto: palavras, caracteres, bytes, sub-palavras
+
+chars = set()
+
+for input in inputs + outputs:
+    for ch in input:
+        if ch not in chars:
+            chars.add(ch)
+
+
+
+print('Número de chars: ',len(chars))
